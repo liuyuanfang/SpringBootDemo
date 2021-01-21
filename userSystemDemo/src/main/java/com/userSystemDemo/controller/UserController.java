b93a765fc2396746d5565287cc1f4cee128b2ed2
刘元芳 <18339982700@163.com>
2021-01-21 22:39:11 +0800

    Update UserController.java

--- userSystemDemo/src/main/java/com/userSystemDemo/controller/UserController.java
+++ userSystemDemo/src/main/java/com/userSystemDemo/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.userSystemDemo.controller;
 
 
+import com.userSystemDemo.dto.UserDTO;
+import com.userSystemDemo.entity.UserEntity;
 import com.userSystemDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// /usr/local/MySQL/bin/mysql -u root -p
+
 
 @RestController
 @RequestMapping("/user")
@@ -16,9 +21,34 @@ public class UserController {
     @Autowired
     UserService userService;
 
+//    @RequestBody 获得请求体
+//    @RequestParam 获得?URL请求头参数值
+    @PostMapping
+    public ResponseEntity<?> addUser(@RequestBody UserDTO userDTO){
+        userService.addUser(userDTO);
+        return new ResponseEntity( HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody UserEntity userEntity){
+        int id = userEntity.getId();
+        String userName = userEntity.getUserName();
+        String pwd = userEntity.getPwd();
+
+        userService.updateUserInfo(id,userName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     @GetMapping
     public ResponseEntity<?> getUser(){
-        return new ResponseEntity(userService.getUser(), HttpStatus.OK);
+        List getALluser = userService.getUser();
+        return new ResponseEntity(getALluser, HttpStatus.OK);
+    }
 
+//    @PathVariable 获得URL路径参数
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?>  deleteUser(@PathVariable int id){
+        userService.deleteUser(id);
+        return new ResponseEntity( HttpStatus.OK);
     }
 }
