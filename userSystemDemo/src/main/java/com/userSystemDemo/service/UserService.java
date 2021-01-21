a2ee4883c822a69f821ac500cfea539eed90ee5a
刘元芳 <18339982700@163.com>
2021-01-21 22:36:56 +0800

    Update UserService.java

--- userSystemDemo/src/main/java/com/userSystemDemo/service/UserService.java
+++ userSystemDemo/src/main/java/com/userSystemDemo/service/UserService.java
@@ -1,8 +1,18 @@
 package com.userSystemDemo.service;
 
+import com.userSystemDemo.dto.UserDTO;
+import com.userSystemDemo.entity.UserEntity;
+
 import java.util.List;
 
 public interface UserService {
 
-     String getUser();
+     List<UserEntity> getUser();
+
+     void addUser(UserDTO userDTO);
+
+     void updateUserInfo(int id,String userName);
+
+     void deleteUser(int id);
 }
+
