e24bd1b89bb8e719778e6c5fd40ca663af2d6e07
刘元芳 <18339982700@163.com>
2021-01-23 18:21:43 +0800

    Update ZooWorkerMapper.java

--- ZooManagementSystem/src/main/java/com/ZooManagementSystem/mapper/ZooWorkerMapper.java
+++ ZooManagementSystem/src/main/java/com/ZooManagementSystem/mapper/ZooWorkerMapper.java
@@ -1,26 +1,24 @@
 package com.ZooManagementSystem.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import com.ZooManagementSystem.entity.ZooWorkerEntity;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface ZooWorkerMapper {
+    
+    List<ZooWorkerEntity> getAllWorker();
 
-    @Select("select * from t_zoo_worker_info")
-    List getAllWorker();
 
-//    @Insert("insert into t_zoo_worker_info(worker_name,worker_pwd,worker_num,worker_position,worker_join_zoo_time) " +
-//            "values (#{worker_name},#{worker_pwd},#{worker_num},#{worker_position},#{worker_join_zoo_time})")
-//    void addWorker();
-//    void addWorker(@Param("worker_name") String username,
-//                   @Param("worker_pwd") String pwd,
-//                   @Param("worker_num") String worker_num,
-//                   @Param("worker_position") String worker_position,
-//                   @Param("worker_join_zoo_time") String worker_join_zoo_time);
+    @Insert("insert into t_zoo_worker_info(workerName,pwd,joinZooTime,workerDepartment,workerPosition) values(#{workerName},#{pwd},#{joinZooTime},#{workerDepartment},#{workerPosition})")
+    ZooWorkerEntity addWorker(@Param("workerName") String workerName,
+                   @Param("pwd") String pwd,
+                   @Param("joinZooTime") String joinZooTime,
+                   @Param("workerDepartment") String workerDepartment,
+                   @Param("workerPosition") String workerPosition);
 
-//    void deleteWorker();
+
+    @Delete("delete from t_zoo_worker_info where workerNumber=#{workerNumber}")
+    void deleteWorker(@Param("workerNumber") int workerNumber);
 }
